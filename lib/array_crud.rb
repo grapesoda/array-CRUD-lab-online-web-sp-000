bf2bd4366915f41d4e0377d9bb749f4e19bbf1a5
Tres Tarwater <trest03@gmail.com>
2020-10-27 20:34:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["array", "element"]
+  ["red", "blue", "green", "black"]
+  .push("array", "element")
 end
 
 def add_element_to_start_of_array(array, element)
