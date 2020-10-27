1e51221fb78a93bb97798d129aaaede1336b354a
Tres Tarwater <trest03@gmail.com>
2020-10-27 20:38:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,30 +8,30 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["red", "blue", "green", "black"]
-  add_element_to_end_of_array.push "array", "element"
+  add_element_to_end_of_array.push("array", "element")
   puts add_element_to_end_of_array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
-  ["red", "blue", "green", "black"]
+  
 end
 
 def remove_element_from_end_of_array(array)
-  ["red", "blue", "green", "black"]
+  
 end
 
 def remove_element_from_start_of_array(array)
-  ["red", "blue", "green", "black"]
+  
 end
 
 def retrieve_element_from_index(array, index_number)
-  ["red", "blue", "green", "black"]
+  
 end
 
 def retrieve_first_element_from_array(array)
-  ["red", "blue", "green", "black"]
+  
 end
 
 def retrieve_last_element_from_array(array)
-  ["red", "blue", "green", "black"]
+  
 end
