183dbf2b8fa3c28a8d0565ab4d14a2262566f8f9
Tres Tarwater <trest03@gmail.com>
2020-10-27 21:37:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["I", "am", "really", "learning"]
     element = "arrays!"
-    array.push(element)
+    Array << Element
    puts array.inspect
 end
  
